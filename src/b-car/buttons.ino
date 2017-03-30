5abecc31899a236df7a7ca80358b0b7966fc2d03
copyrights <copyrights@gmx.de>
2017-03-30 21:27:44 +0200

    add software debouce function to R2R network

--- src/b-car/buttons.ino
+++ src/b-car/buttons.ino
@@ -1,6 +1,11 @@
 /*
 R2R network
 */
+#define DEBOUNCE 50
+uint16_t lastbuttons = 0;
+uint16_t stablebuttons = 0;
+unsigned long dtime = 0;
+
 uint16_t pressedButtons()
 {
   uint16_t reading, rest;
@@ -11,7 +16,24 @@ uint16_t pressedButtons()
     reading = reading + 32;
   reading = reading & 0x3e0;
   reading = reading >> 5;
+  reading = debounce(reading);
   return reading;
 }
 
+
+uint16_t debounce(uint16_t curbuttons)
+{
+  if (curbuttons == lastbuttons)
+  {
+    if(stablebuttons != lastbuttons && (time-dtime) > DEBOUNCE){
+        stablebuttons = lastbuttons;
+    }
+  }
+  else
+  {
+    lastbuttons = curbuttons;
+    dtime = time;
+  }
+  return stablebuttons;
+}
 
